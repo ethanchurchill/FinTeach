a70f9e05cbd8dc9e4facd0cf5eba52ee1a65899c
rishinair21 <rishinair21@gmail.com>
2021-10-20 16:19:59 -0400

    Added landing page and navbar components

--- FinTeach/src/app/app.module.ts
+++ FinTeach/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NavBar } from './navbar/navbar.component'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBar
   ],
   imports: [
     BrowserModule,

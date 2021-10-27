6d82fa1a29e353ea7a98a3d5a3c404a5f33a2b93
Ohad Nir <45518501+OhadNir@users.noreply.github.com>
2021-10-27 18:48:12 -0400

    routerLink

--- FinTeach/src/app/app-routing.module.ts
+++ FinTeach/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ModulesComponent } from './modules/modules.component';
+import { TermsComponent } from './terms/terms.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'modules', component: ModulesComponent },
+  { path: 'terms', component: TermsComponent },
+  { path: 'dashboard', component: DashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

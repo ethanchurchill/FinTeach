bd76138b3e6fe52bcbe41cb22239fe8037fe81d5
Ohad Nir <45518501+OhadNir@users.noreply.github.com>
2021-10-28 22:14:49 -0400

    Fixed routing issue.

--- FinTeach/src/app/app-routing.module.ts
+++ FinTeach/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { ModulesComponent } from './modules/modules.component';
 import { TermsComponent } from './terms/terms.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 const routes: Routes = [
   { path: 'modules', component: ModulesComponent },

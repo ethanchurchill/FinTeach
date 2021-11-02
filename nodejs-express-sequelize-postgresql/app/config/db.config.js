724a720a0862c6d6af41471eb86cc06a5cd38fff
rishinair21 <rishinair21@gmail.com>
2021-11-02 18:17:01 -0400

    Node express server for backend integration

---
+++ nodejs-express-sequelize-postgresql/app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "rishinair",
+    PASSWORD: "Tridrum19!",
+    DB: "finteach_db",
+    dialect: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };

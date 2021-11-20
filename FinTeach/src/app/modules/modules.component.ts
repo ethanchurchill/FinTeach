3593eac4d8018eb8c21b9b9c5956721fdb659faf
Ohad Nir <45518501+OhadNir@users.noreply.github.com>
2021-11-19 19:21:46 -0500

    Linked submodule page to module block.

--- FinTeach/src/app/modules/modules.component.ts
+++ FinTeach/src/app/modules/modules.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Module } from 'src/app/models/module-model';
 import { ModuleService } from 'src/app/services/module-service';
 
+
 @Component({
   selector: 'app-modules',
   templateUrl: './modules.component.html',
@@ -11,24 +12,24 @@ export class ModulesComponent implements OnInit {
   dummyModules?: Module[]; //Dummy data for now
   modules?: Module[];
   showModule: any;
-  constructor(private moduleService: ModuleService, private modalService: NgbModal) { }
+  constructor(private moduleService: ModuleService) { }
   ngOnInit(): void {
     this.initializeDummyModules();
     this.retrieveModules();
   }
   initializeDummyModules(): void {
     const dummy1 = new Module();
-    dummy1.name = "Stocks"
-    dummy1.description = "This module covers stocks. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    dummy1.name = "Stocks";
+    dummy1.description = "This module covers stocks. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
     dummy1.img_ref = "/assets/stocks-folder.png"
     const dummy2 = new Module();
-    dummy2.name = "Budgeting"
-    dummy2.description = "This module covers budgeting. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-    dummy2.img_ref = "/assets/budgeting-folder.png"
+    dummy2.name = "Budgeting";
+    dummy2.description = "This module covers budgeting. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est; laborum";
+    dummy2.img_ref = "/assets/budgeting-folder.png";
     const dummy3 = new Module();
-    dummy3.name = "Credit"
-    dummy3.description = "This module covers credit. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-    dummy3.img_ref = "/assets/credit-folder.png"
+    dummy3.name = "Credit";
+    dummy3.description = "This module covers credit. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+    dummy3.img_ref = "/assets/credit-folder.png";
 
     this.dummyModules = [dummy1, dummy2, dummy3]
   }
